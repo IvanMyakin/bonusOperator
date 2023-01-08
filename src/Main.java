5120f81866b437eb63b8020e29756ac31732bc52
Ivan Myakinkin <myakinkin.iwan@yandex.ru>
2023-01-08 16:45:52 +0700

    charge bonusOperator

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int initialAccount = 300; // деньги на счету клиента
-        int depositAmount = 1200; // сумма пополнения
+        int depositAmount = 1500; // сумма пополнения
         int bonus;
-        if (depositAmount >= 1000) {
+        if (depositAmount > 1000) {
             bonus = depositAmount / 100 * 1 + initialAccount + depositAmount;
             System.out.println(bonus);
         } else {
             System.out.println(initialAccount + depositAmount + " " + "Бонусы не начислены");
         }
-    System.out.println("Спасибо за пополнение счета!");
+        System.out.println("Спасибо за пополнение счета!");
     }
 }
